cd039964c4b78eb2b97f9ea8c8c6e3a47640e30f
reigadegr <2722688642@qq.com>
2024-08-10 13:32:08 +0800

    优化脚本一键编译

--- build.sh
+++ build.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 mkdir -p output
 rm -rf output/*
-cargo b -r --target aarch64-linux-android
+# cargo b -r --target aarch64-linux-android
+cargo +nightly ndk -p 34 -t arm64-v8a build --target aarch64-linux-android -Z build-std --release
 cd output
 cp -r ../module/* .
-cp -f ../target/aarch64-linux-android/release/dangerous .
+dd if=../target/aarch64-linux-android/release/dangerous of=./dangerous
 strip ./dangerous
 zip -9 -rq dangerous.zip .
