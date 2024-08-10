bfbddde701bf84fde5f923aba887f6a9facaa44a
reigadegr <2722688642@qq.com>
2024-08-10 13:34:02 +0800

    更新模块启动脚本

--- module/service.sh
+++ module/service.sh
@@ -1,3 +1,17 @@
 MODDIR=${0%/*}
+wait_until_login() {
+    # in case of /data encryption is disabled
+    while [ "$(getprop sys.boot_completed)" != "1" ]; do
+        sleep 1
+    done
 
-$MODDIR/dangerous
+    # in case of the user unlocked the screen
+    while [ ! -d "/sdcard/Android" ]; do
+        sleep 1
+    done
+}
+
+wait_until_login
+chmod a+x dangerous
+killall -15 dangerous
+nohup $MODDIR/dangerous >/dev/null 2>&1 &
